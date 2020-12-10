51b3ed67f70d66d5f1dcfcc99aefafb63165b151
jjm <whdals6831@gmail.com>
2020-12-10 20:46:54 +0900

    cors2

--- hproject/settings.py
+++ hproject/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    ' corsheaders ' ,
+    'corsheaders' ,
     'happ',
 ]
 
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    ' corsheaders.middleware.CorsMiddleware ' ,
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
